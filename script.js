aea02a1f7a8514ca9e0930fcfa88e72eced33a4f
Erisan Rakipi <ersian813@gmail.com>
2023-06-22 16:07:24 +0200

    frist commit

--- script.js
+++ script.js
@@ -1,40 +1,78 @@
 let weather = {
-    'apiKey': '56a7ce6ab3c1d3c509e06587b4bafe71',
-    fetchWeather : function(city) {
-      fetch(
-        'https://api.openweathermap.org/data/2.5/weather?q='
-        + city
-        +'&units=metric&appid=' 
-        + this.apiKey
-      ).then((response) => response.json())
-      .then((data) => this.displayWeather(data))
-    },
-    displayWeather: function (data){
-      const { name } = data;
-      const { icon , description } = data.weather[0];
-      const { temp, humidity } =data.main;
-      const { speed } = data.wind;
-      const { country } = data.sys;
-  
-      console.log(name, icon, description, temp, humidity, speed , country)
-  
-      document.querySelector('.city').innerHTML =  name;
-      document.querySelector('.icon').src = 'https://openweathermap.org/img/wn/'+ icon + '@4x.png';
-      document.querySelector('.description').innerHTML = description;
-      document.querySelector('.temp').innerHTML =  Math.floor(temp)  + '°C';
-      document.querySelector('.humidity').innerHTML = 'Humidity ' + humidity + '%'
-      document.querySelector('.wind').innerHTML = 'Wind speed ' + speed + 'km/h'
-      document.querySelector('.country').innerHTML= country
-      
-    },
-  
-    search: function(){
-     this.fetchWeather( document.querySelector('.search-bar').value);
-    }
-  }
-  
-  document.querySelector('.search button').addEventListener('click', function () {
-    weather.search();
-  })
+  apiKey: '56a7ce6ab3c1d3c509e06587b4bafe71',
+  fetchWeather: function(city) {
+    fetch(
+      'https://api.openweathermap.org/data/2.5/weather?q=' +
+        city +
+        '&units=metric&appid=' +
+        this.apiKey
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.cod === '404') {
+          this.displayErrorMessage();
+        } else {
+          this.displayWeather(data);
+        }
+      });
+  },
+  displayWeather: function(data) {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+    const { country } = data.sys;
 
-  console.log(weather)
+    document.querySelector('.city').innerHTML = name;
+    document.querySelector('.country').innerHTML = country;
+    document.querySelector('.temp').innerHTML = Math.floor(temp) + '°C';
+    document.querySelector('.icon').src =
+      'https://openweathermap.org/img/wn/' + icon + '.png';
+    document.querySelector('.description').innerHTML = description;
+    document.querySelector('.humidity').innerHTML = 'Humidity: ' + humidity + '%';
+    document.querySelector('.wind').innerHTML = speed + ' km/h';
+
+    // Remove error message if it was previously displayed
+    const errorMessage = document.querySelector('.error-message');
+    if (errorMessage) {
+      errorMessage.classList.remove('show');
+    }
+  },
+  displayErrorMessage: function() {
+    // Remove existing error message if it was previously displayed
+    const errorMessage = document.querySelector('.error-message');
+    if (errorMessage) {
+      errorMessage.remove();
+    }
+
+    // Create a new error message element
+    const errorElement = document.createElement('div');
+    errorElement.classList.add('error-message');
+    errorElement.textContent = 'The entered city does not exist. Please try again.';
+
+    // Append the error message element to the document body
+    document.querySelector('.card').appendChild(errorElement);
+
+    // Show the error message
+    errorElement.classList.add('show');
+  },
+  search: function() {
+    const searchInput = document.querySelector('.search-bar');
+    const city = searchInput.value.trim();
+
+    if (city) {
+      this.fetchWeather(city);
+      searchInput.value = '';
+    }
+  },
+};
+
+document.querySelector('.btn').addEventListener('click', function() {
+  weather.search();
+});
+
+document.querySelector('.search-bar').addEventListener('keydown', function(event) {
+  if (event.key === 'Enter') {
+    weather.search();
+  }
+});
